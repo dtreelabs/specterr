f2df1e10f3afeaa5ac320725ef3ab4e44482964c
Gaurav Singh <gaurav.singh@coupa.com>
2018-10-13 11:25:58 +0530

    basic framework implemented

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  get 'test/list'
+
+  get 'test/index'
+
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
