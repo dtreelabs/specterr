1670702b3f05a29cb2d455644856b1d1ba9b2b89
kapil <kapilbhosale@hotmail.com>
2018-11-21 17:54:21 +0530

    web ui changes

--- lib/specterr/web.rb
+++ lib/specterr/web.rb
@@ -1,15 +1,25 @@
 # frozen_string_literal: true
 require 'erb'
 require 'rack'
+require 'rack/builder'
+require 'rack/deflater'
 
 module Specterr
   class Web
+    ROOT = File.expand_path("#{File.dirname(__FILE__)}/../../web")
+    VIEWS = "#{ROOT}/views"
+    LAYOUT = "#{VIEWS}/layout.erb"
+    ASSETS = "#{ROOT}/assets"
+
     def self.call(env)
       req = Rack::Request.new(env)
       case req.path_info
-        when //
-          [200, {"Content-Type" => "text/html"}, ["Root page"]]
-        when /hello/
+        when '/'
+          @name = 'kapil bhosale'
+          template = File.read("#{VIEWS}/index.html.erb")
+          content = render(template)
+          ['200', {"Content-Type" => "text/html"}, [content]]
+        when '/hello'
           [200, {"Content-Type" => "text/html"}, ["Hello World!"]]
         when /goodbye/
           [500, {"Content-Type" => "text/html"}, ["Goodbye Cruel World!"]]
@@ -17,7 +27,32 @@ module Specterr
           [404, {"Content-Type" => "text/html"}, ["I'm Lost!"]]
       end
     end
+
+    def self.settings
+      {
+          root: ROOT,
+          views: VIEWS
+      }
+    end
+
+    private
+    def self.render(template)
+      layout = File.read("#{VIEWS}/layout.html.erb")
+      [template, layout].inject(nil) do | prev, temp |
+        _render(temp) { prev }
+      end
+    end
+
+    def self._render temp
+      ERB.new(temp).result(binding)
+    end
   end
 end
 
 
+# app = Rack::Builder.new do
+#   use Rack::Deflater        # GZip
+#   run Specterr::Web
+# end
+#
+# Rack::Server.start :app => app
